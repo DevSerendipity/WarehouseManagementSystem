0f72e7c72064c2c595815be2df8d1261332616e0
IslamSakrak <islamsakrak2002@gmail.com>
2021-11-08 12:52:20 +0100

    Changing the database and refactoring the code to be more clear

--- main/java/LMS/Connection/Connect.java
+++ main/java/LMS/Connection/Connect.java
@@ -1,45 +1,21 @@
 package LMS.Connection;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connect {
-    //WORKED https://www.sqlitetutorial.net/sqlite-java/insert/
-    // create a new db tomorrow and go all over again
 
-    /**
-     * Connect to a sample database
-     */
     public static void connect() {
-        Connection con = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:**PATH TO DATABASE**";
-
-            // create a connection to the database
-            con = DriverManager.getConnection(url);
-
-            System.out.println("Connection to SQLite has been established.");
-
+        String url = "jdbc:postgresql://localhost/warehouse";
+        try (Connection connection = DriverManager.getConnection(url, "islam", "learningDatabase")) {
+            if (connection != null) {
+                System.out.println("Connection to PostgreSQL has been established.");
+            } else {
+                System.out.println("failed to connect");
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        connect();
     }
 }
