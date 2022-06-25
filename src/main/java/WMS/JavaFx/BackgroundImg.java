72c3e437c3a327fe52aaed0808eb575fc728674d
FoolProof <islamsakrak2002@gmail.com>
2022-06-25 15:11:40 +0200

    Renaming iml and reflecting that into git ignore

--- src/main/java/WMS/JavaFx/BackgroundImg.java
+++ src/main/java/WMS/JavaFx/BackgroundImg.java
@@ -12,7 +12,7 @@ import java.io.FileNotFoundException;
 
 public class BackgroundImg {
     public Background getBackgroundImage() throws FileNotFoundException {
-        FileInputStream input = new FileInputStream("src/main/resources/Test6.jpeg");
+        FileInputStream input = new FileInputStream("src/main/resources/background.jpeg");
         Image image = new Image(input);
 
         BackgroundImage backgroundimage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(1.0, 1.0, true, true, false, false));
