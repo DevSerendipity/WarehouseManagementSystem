34baf37b50d23d44f672c006e1e9077f7a3732a2
FoolProof <islamsakrak2002@gmail.com>
2022-06-26 18:45:16 +0200

    Fixing capitalization in package names

--- src/main/java/WMS/JavaFx/BackgroundImg.java
+++ src/main/java/WMS/JavaFx/BackgroundImg.java
@@ -1,4 +1,4 @@
-package WMS.JavaFx;
+package wms.javafx;
 
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
@@ -12,10 +12,13 @@ import java.io.FileNotFoundException;
 
 public class BackgroundImg {
     public Background getBackgroundImage() throws FileNotFoundException {
-        FileInputStream input = new FileInputStream("src/main/resources/background.jpeg");
-        Image image = new Image(input);
+        FileInputStream input = new FileInputStream( "src/main/resources/background.jpeg" );
+        Image image = new Image( input );
 
-        BackgroundImage backgroundimage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(1.0, 1.0, true, true, false, false));
-        return new Background(backgroundimage);
+        BackgroundImage backgroundimage = new BackgroundImage( image, BackgroundRepeat.NO_REPEAT,
+                                                               BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                                                               new BackgroundSize( 1.0, 1.0, true, true, false,
+                                                                                   false ) );
+        return new Background( backgroundimage );
     }
 }
