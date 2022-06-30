4707e0d34f22fe682c6970decd0f5d16ddea9ed7
FoolProof <islamsakrak2002@gmail.com>
2022-06-30 18:03:36 +0200

    Erasing the little -><- switch statements

--- src/main/java/WMS/JavaFx/event/CompanyEvent.java
+++ src/main/java/WMS/JavaFx/event/CompanyEvent.java
@@ -1,6 +1,7 @@
 package wms.javafx.event;
 
 import wms.javafx.ComboBoxProperties;
+import wms.javafx.Company;
 
 public final class CompanyEvent {
 
@@ -8,58 +9,36 @@ public final class CompanyEvent {
 
     }
 
-    private static int companyCode;
-    //what??
-    private static final char[] FIRST_ASCI_LETTER_OF_COMPANY = new char[ 1 ];
-    private static final char[] SECOND_ASCI_LETTER_OF_COMPANY = new char[ 1 ];
-
-    public static void getCompanyCodeEvent() {
-        ComboBoxProperties.getCompanies().getSelectionModel().selectedItemProperty()
-                .addListener( ( selectedCompany, oldCompany, newCompany ) -> {
-                    if ( newCompany != null ) {
-                        switch ( newCompany ) {
-                            case "Volkswagen Group" -> companyCode = 100;
-                            case "Daimler" -> companyCode = 101;
-                            case "Airbus" -> companyCode = 102;
-                            case "Anheuser-Busch InBev" -> companyCode = 103;
-                            case "BASF" -> companyCode = 104;
-                            case "Apple" -> companyCode = 105;
-                            case "Boeing" -> companyCode = 106;
-                            case "Cardinal Health" -> companyCode = 107;
-                            case "Dell" -> companyCode = 108;
-                            case "China Baowu Steel Group" -> companyCode = 109;
-                            case "DowDuPont" -> companyCode = 110;
-                            case "Foxconn" -> companyCode = 111;
-                            case "General Electronics" -> companyCode = 112;
-                            case "General Motors" -> companyCode = 113;
-                            case "Hoffmann-La Roche" -> companyCode = 114;
-                            case "JBS S.A." -> companyCode = 115;
-                            case "Nippon Steel & Sumitomo Metal" -> companyCode = 116;
-                            case "Norinco" -> companyCode = 117;
-                            case "PepsiCo" -> companyCode = 118;
-                            case "Pfizer" -> companyCode = 119;
-                            case "POSCO" -> companyCode = 120;
-                            case "Samsung Electronics" -> companyCode = 121;
-                            case "Sinochem" -> companyCode = 122;
-                            case "Toyota Group" -> companyCode = 123;
-                            default -> companyCode = 0;
-                        }
-                    }
-                    assert newCompany != null;
-                    FIRST_ASCI_LETTER_OF_COMPANY[ 0 ] = newCompany.toUpperCase().charAt( 0 );
-                    SECOND_ASCI_LETTER_OF_COMPANY[ 0 ] = newCompany.toUpperCase().charAt( newCompany.length() - 1 );
-                } );
-    }
-
     public static int getCompanyCode() {
         return companyCode;
     }
 
-    public static int getFirstAsciLetterOfCompany( int index ) {
-        return FIRST_ASCI_LETTER_OF_COMPANY[ index ];
+    private static int companyCode;
+    private static char FIRST_LETTER_OF_COMPANY_NAME = ' ';
+    private static char LAST_LETTER_OF_COMPANY_NAME = ' ';
+
+    public static void getCompanyCodeEvent() {
+        ComboBoxProperties.getCompanies().getSelectionModel().selectedItemProperty()
+                .addListener( ( selectedCompany, oldCompany, newCompany ) -> {
+                    System.out.println( selectedCompany );
+                    System.out.println( oldCompany );
+                    System.out.println( newCompany );
+                    for ( Company s: Company.values() ) {
+                        if ( newCompany.equals( s.getCompanyName() ) ) {
+                            companyCode = s.getCompanyCode();
+                            FIRST_LETTER_OF_COMPANY_NAME = newCompany.toUpperCase().charAt( 0 );
+                            LAST_LETTER_OF_COMPANY_NAME = newCompany.toUpperCase().charAt( newCompany.length() - 1 );
+                        }
+                    }
+                } );
     }
 
-    public static int getSecondAsciLetterOfCompany( int index ) {
-        return SECOND_ASCI_LETTER_OF_COMPANY[ index ];
+
+    public static char getFirstLetterOfCompanyName(  ) {
+        return FIRST_LETTER_OF_COMPANY_NAME;
+    }
+
+    public static int getLastLetterOfCompanyName(  ) {
+        return LAST_LETTER_OF_COMPANY_NAME;
     }
 }
